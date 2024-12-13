7c689260c87d91ed749acd731c9c703067a04cf4
Holly Cummins <hcummins@redhat.com>
2024-12-13 14:02:58 +1100

    Correct name of service loader file, but how did it ever work before?

--- test-framework/junit5/src/main/java/io/quarkus/test/junit/QuarkusTestExtension.java
+++ test-framework/junit5/src/main/java/io/quarkus/test/junit/QuarkusTestExtension.java
@@ -1109,6 +1109,8 @@ public class QuarkusTestExtension extends AbstractJvmQuarkusTestExtension
                 }
             }
 
+            // TODO is all this reflection even needed?
+
             if (testMethodInvokerToUse != null) {
                 return testMethodInvokerToUse.getClass()
                         .getMethod("invoke", Object.class, Method.class, List.class, String.class)
