45ce48e538be1f1fafae6bc50b06ee790b9ad248
Holly Cummins <hcummins@redhat.com>
2025-02-03 17:03:07 +0000

    Format

--- integration-tests/reactive-messaging-context-propagation/src/test/java/io/quarkus/it/kafka/KafkaContextPropagationTest.java
+++ integration-tests/reactive-messaging-context-propagation/src/test/java/io/quarkus/it/kafka/KafkaContextPropagationTest.java
@@ -79,24 +79,24 @@ public class KafkaContextPropagationTest {
     @Test
     void testAbsenceOfContextPropagation() {
         given().body("rose").post("/flowers").then()
-               .statusCode(500)
-               .body(assertBodyRequestScopedContextWasNotActive());
+                .statusCode(500)
+                .body(assertBodyRequestScopedContextWasNotActive());
     }
 
     @Order(1)
     @Test
     void testAbsenceOfContextPropagationUni() {
         given().body("rose").post("/flowers/uni").then()
-               .statusCode(500)
-               .body(assertBodyRequestScopedContextWasNotActive());
+                .statusCode(500)
+                .body(assertBodyRequestScopedContextWasNotActive());
     }
 
     @Order(50)
     @Test
     void testAbsenceOfContextPropagationBlocking() {
         given().body("rose").post("/flowers/blocking").then()
-               .statusCode(500)
-               .body(assertBodyRequestScopedContextWasNotActive());
+                .statusCode(500)
+                .body(assertBodyRequestScopedContextWasNotActive());
     }
 
     @Order(25)
@@ -105,24 +105,24 @@ public class KafkaContextPropagationTest {
         System.out.println("HOLLY order confirmation" + "testAbsenceOfContextPropagationBlockingUni");
 
         given().body("rose").post("/flowers/uni/blocking").then()
-               .statusCode(500)
-               .body(assertBodyRequestScopedContextWasNotActive());
+                .statusCode(500)
+                .body(assertBodyRequestScopedContextWasNotActive());
     }
 
     @Order(1)
     @Test
     void testAbsenceOfContextPropagationBlockingNamed() {
         given().body("rose").post("/flowers/blocking-named").then()
-               .statusCode(500)
-               .body(assertBodyRequestScopedContextWasNotActive());
+                .statusCode(500)
+                .body(assertBodyRequestScopedContextWasNotActive());
     }
 
     @Order(1)
     @Test
     void testAbsenceOfContextPropagationBlockingNamedUni() {
         given().body("rose").post("/flowers/uni/blocking-named").then()
-               .statusCode(500)
-               .body(assertBodyRequestScopedContextWasNotActive());
+                .statusCode(500)
+                .body(assertBodyRequestScopedContextWasNotActive());
     }
 
     @Order(3)
@@ -130,8 +130,8 @@ public class KafkaContextPropagationTest {
     @EnabledForJreRange(min = JRE.JAVA_21)
     void testAbsenceOfContextPropagationVirtualThread() {
         given().body("rose").post("/flowers/virtual-thread").then()
-               .statusCode(500)
-               .body(assertBodyRequestScopedContextWasNotActive());
+                .statusCode(500)
+                .body(assertBodyRequestScopedContextWasNotActive());
     }
 
     @Order(1)
@@ -139,8 +139,8 @@ public class KafkaContextPropagationTest {
     @EnabledForJreRange(min = JRE.JAVA_21)
     void testAbsenceOfContextPropagationVirtualThreadUni() {
         given().body("rose").post("/flowers/uni/virtual-thread").then()
-               .statusCode(500)
-               .body(assertBodyRequestScopedContextWasNotActive());
+                .statusCode(500)
+                .body(assertBodyRequestScopedContextWasNotActive());
     }
 
     protected Matcher<String> assertBodyRequestScopedContextWasNotActive() {
@@ -151,14 +151,14 @@ public class KafkaContextPropagationTest {
     @Test
     void testIncomingFromConnector() {
         given().body("rose").post("/flowers/produce").then()
-               .statusCode(204);
+                .statusCode(204);
         given().body("daisy").post("/flowers/produce").then()
-               .statusCode(204);
+                .statusCode(204);
         given().body("peony").post("/flowers/produce").then()
-               .statusCode(204);
+                .statusCode(204);
 
         await().pollDelay(5, TimeUnit.SECONDS).untilAsserted(() -> given().get("/flowers/received")
-                                                                          .then().body(not(containsString("rose")),
+                .then().body(not(containsString("rose")),
                         not(containsString("daisy")),
                         not(containsString("peony"))));
     }
