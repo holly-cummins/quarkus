023e8017a7e2fb4960e224799f50d4d05f153b23
Holly Cummins <hcummins@redhat.com>
2025-02-03 19:43:57 +0000

    Order which is likely to pass

--- integration-tests/reactive-messaging-context-propagation/src/test/java/io/quarkus/it/kafka/KafkaContextPropagationTest.java
+++ integration-tests/reactive-messaging-context-propagation/src/test/java/io/quarkus/it/kafka/KafkaContextPropagationTest.java
@@ -24,138 +24,126 @@ import io.quarkus.test.kafka.KafkaCompanionResource;
 @QuarkusTestResource(KafkaCompanionResource.class)
 public class KafkaContextPropagationTest {
 
-    @Order(1)
     @Test
     void testContextPropagation() {
         given().body("rose").post("/flowers/contextual").then().statusCode(204);
     }
 
-    @Order(1)
     @Test
     void testContextPropagationUni() {
         given().body("rose").post("/flowers/contextual/uni").then().statusCode(204);
     }
 
-    @Order(1)
     @Test
     void testContextPropagationBlocking() {
         given().body("rose").post("/flowers/contextual/blocking").then().statusCode(204);
     }
 
-    @Order(1)
     @Test
     void testContextPropagationBlockingUni() {
         given().body("rose").post("/flowers/contextual/uni/blocking").then().statusCode(204);
     }
 
-    @Order(1)
     @Test
     void testContextPropagationBlockingNamed() {
         given().body("rose").post("/flowers/contextual/blocking-named").then().statusCode(204);
     }
 
-    @Order(1)
     @Test
     void testContextPropagationBlockingNamedUni() {
         given().body("rose").post("/flowers/contextual/uni/blocking-named").then().statusCode(204);
     }
 
-    @Order(1)
     @Test
     @EnabledForJreRange(min = JRE.JAVA_21)
     void testContextPropagationVirtualThread() {
         given().body("rose").post("/flowers/contextual/virtual-thread").then().statusCode(204);
     }
 
-    @Order(4)
     @Test
     @EnabledForJreRange(min = JRE.JAVA_21)
     void testContextPropagationVirtualThreadUni() {
         given().body("rose").post("/flowers/contextual/uni/virtual-thread").then().statusCode(204);
     }
 
-    @Order(300)
+    @Order(3)
     @Test
     void testAbsenceOfContextPropagation() {
         given().body("rose").post("/flowers").then()
-                .statusCode(500)
-                .body(assertBodyRequestScopedContextWasNotActive());
+               .statusCode(500)
+               .body(assertBodyRequestScopedContextWasNotActive());
     }
 
-    @Order(1)
     @Test
     void testAbsenceOfContextPropagationUni() {
         given().body("rose").post("/flowers/uni").then()
-                .statusCode(500)
-                .body(assertBodyRequestScopedContextWasNotActive());
+               .statusCode(500)
+               .body(assertBodyRequestScopedContextWasNotActive());
     }
 
-    @Order(50)
+    @Order(5)
     @Test
     void testAbsenceOfContextPropagationBlocking() {
         given().body("rose").post("/flowers/blocking").then()
-                .statusCode(500)
-                .body(assertBodyRequestScopedContextWasNotActive());
+               .statusCode(500)
+               .body(assertBodyRequestScopedContextWasNotActive());
     }
 
-    @Order(75)
+    @Order(7)
     @Test
     void testAbsenceOfContextPropagationBlockingUni() {
         given().body("rose").post("/flowers/uni/blocking").then()
-                .statusCode(500)
-                .body(assertBodyRequestScopedContextWasNotActive());
+               .statusCode(500)
+               .body(assertBodyRequestScopedContextWasNotActive());
     }
 
-    @Order(1)
     @Test
     void testAbsenceOfContextPropagationBlockingNamed() {
         given().body("rose").post("/flowers/blocking-named").then()
-                .statusCode(500)
-                .body(assertBodyRequestScopedContextWasNotActive());
+               .statusCode(500)
+               .body(assertBodyRequestScopedContextWasNotActive());
     }
 
-    @Order(1)
     @Test
     void testAbsenceOfContextPropagationBlockingNamedUni() {
         given().body("rose").post("/flowers/uni/blocking-named").then()
-                .statusCode(500)
-                .body(assertBodyRequestScopedContextWasNotActive());
+               .statusCode(500)
+               .body(assertBodyRequestScopedContextWasNotActive());
     }
 
-    @Order(3)
+    @Order(10)
     @Test
     @EnabledForJreRange(min = JRE.JAVA_21)
     void testAbsenceOfContextPropagationVirtualThread() {
         given().body("rose").post("/flowers/virtual-thread").then()
-                .statusCode(500)
-                .body(assertBodyRequestScopedContextWasNotActive());
+               .statusCode(500)
+               .body(assertBodyRequestScopedContextWasNotActive());
     }
 
-    @Order(1)
+    @Order(8)
     @Test
     @EnabledForJreRange(min = JRE.JAVA_21)
     void testAbsenceOfContextPropagationVirtualThreadUni() {
         given().body("rose").post("/flowers/uni/virtual-thread").then()
-                .statusCode(500)
-                .body(assertBodyRequestScopedContextWasNotActive());
+               .statusCode(500)
+               .body(assertBodyRequestScopedContextWasNotActive());
     }
 
     protected Matcher<String> assertBodyRequestScopedContextWasNotActive() {
         return containsString("RequestScoped context was not active");
     }
 
-    @Order(1)
     @Test
     void testIncomingFromConnector() {
         given().body("rose").post("/flowers/produce").then()
-                .statusCode(204);
+               .statusCode(204);
         given().body("daisy").post("/flowers/produce").then()
-                .statusCode(204);
+               .statusCode(204);
         given().body("peony").post("/flowers/produce").then()
-                .statusCode(204);
+               .statusCode(204);
 
         await().pollDelay(5, TimeUnit.SECONDS).untilAsserted(() -> given().get("/flowers/received")
-                .then().body(not(containsString("rose")),
+                                                                          .then().body(not(containsString("rose")),
                         not(containsString("daisy")),
                         not(containsString("peony"))));
     }
