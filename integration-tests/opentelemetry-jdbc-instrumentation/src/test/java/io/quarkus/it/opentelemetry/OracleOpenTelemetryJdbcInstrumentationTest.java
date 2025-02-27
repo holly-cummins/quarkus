80f54ccc3bf2a956de70dc702cd72c75edc48515
Holly Cummins <hcummins@redhat.com>
2025-02-27 13:39:26 +0000

    Hack around otel jdbc db issue

--- integration-tests/opentelemetry-jdbc-instrumentation/src/test/java/io/quarkus/it/opentelemetry/OracleOpenTelemetryJdbcInstrumentationTest.java
+++ integration-tests/opentelemetry-jdbc-instrumentation/src/test/java/io/quarkus/it/opentelemetry/OracleOpenTelemetryJdbcInstrumentationTest.java
@@ -1,12 +1,16 @@
 package io.quarkus.it.opentelemetry;
 
+import io.quarkus.test.junit.TestProfile;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
 
 @QuarkusTest
 @QuarkusTestResource(value = OracleLifecycleManager.class, restrictToAnnotatedClass = true)
+// TODO between Feb 4th and 17th something changed which meant postgres state is contaminating the ability to connect to Oracle if everything shares a ClassLoader
+@TestProfile(OracleOpenTelemetryJdbcInstrumentationTest.SomeProfile.class)
 public class OracleOpenTelemetryJdbcInstrumentationTest extends OpenTelemetryJdbcInstrumentationTest {
 
     @Test
@@ -14,4 +18,8 @@ public class OracleOpenTelemetryJdbcInstrumentationTest extends OpenTelemetryJdb
         testQueryTraced("oracle", "OracleHit");
     }
 
+    public static class SomeProfile implements QuarkusTestProfile {
+        public SomeProfile() {
+        }
+    }
 }
